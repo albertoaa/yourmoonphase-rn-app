3df3603e662c4e187eea81fe050a0cafac1d98bb
Alberto Aragon <ronin8503@gmail.com>
2018-02-26 16:57:05 -0500

    Basic Login Layout

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Button, Image, Text, TextInput, View } from 'react-native';
 
-const styles = require('./LoginStyles')
+const styles = require('./LoginStyles');
 
-export default class Login extends React.Component {
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: 'Enter your username..',
+      password: 'Enter your password...'
+    };
+  }
+
+  signinUser(username, password) {
+    console.log(username + " - " + password);
+  }
+
   render() {
     return (
-      <View id="login-container">
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style = {styles.login_container}>
+        <Image source={require('../../assets/img/logo-white-bg.png')} style = {styles.login_logo}/>
+        <Text>Username:</Text>
+        <TextInput
+          style={styles.login_input}
+          onChangeText={(username) => this.setState({username})}
+          value={this.state.username}
+        />
+        <Text>Password:</Text>
+        <TextInput
+          style={styles.login_input}
+          onChangeText={(password) => this.setState({password})}
+          value={this.state.password}
+        />
+        <Button
+          onPress={this.signinUser(this.state.username, this.state.password)}
+          title="LOGIN"
+          style = {styles.login_button}
+          accessibilityLabel="Login with your username and password"
+        />
       </View>
     );
   }
--- components/Login/LoginStyles.js
+++ components/Login/LoginStyles.js
@@ -2,4 +2,28 @@ const React = require('react-native');
 const { StyleSheet } = React;
 
 module.exports = StyleSheet.create({
+  login_container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  login_logo: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain'
+  },
+  login_input: {
+    width: 200,
+    height: 40,
+    borderColor: 'gray',
+    color: 'gray',
+    borderWidth: 1,
+    marginTop: 5,
+    marginBottom: 20
+  },
+  login_button: {
+    backgroundColor: '#000',
+    color: '#fff'
+  }
 });
