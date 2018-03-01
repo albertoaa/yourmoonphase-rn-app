b1e689d7666faabd6a0093420d0824e2d4101206
Alberto Aragon <ronin8503@gmail.com>
2018-03-01 11:31:31 -0500

    Adding neede packages for Cognito and setting Cognito credentials on Environmen file

--- components/Environment.js
+++ components/Environment.js
@@ -1,6 +1,6 @@
 const Environment = {
-  COGNITO_POOL_ID: 'us-west-2_hUQZ8Zqk7',
-  COGNITO_APP_CLIENT_ID: '4ult2iqtq58sjsgf058r7jmng0'
+  COGNITO_POOL_ID: 'us-east-1_YulW3LqKr',
+  COGNITO_APP_CLIENT_ID: '22uf9o5nsqdfp0fpo3mqnuakmh'
 };
 
 export default Environment;
